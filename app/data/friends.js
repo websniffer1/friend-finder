6556637568922ce6f75e342dcb6c8ac0b5e0b09f
Emmanuel <shellguy112@yahoo.com>
2019-03-26 22:35:31 -0400

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendList = [
+    {
+      name: 'Stefanie Ding',
+      profilePic: 'https://avatars3.githubusercontent.com/u/17751317?v=3&s=466',
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
